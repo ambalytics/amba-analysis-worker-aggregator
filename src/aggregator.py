7db8dc7bea5c5314f092b229e55e076e1e2d9866
Lukas Jesche <ljaschka@gmail.com>
2021-08-30 22:40:58 +0200

    - fixes

--- src/aggregator.py
+++ src/aggregator.py
@@ -1,5 +1,6 @@
 import copy
 import datetime
+import os
 import time
 import types
 import uuid
@@ -7,16 +8,18 @@ from datetime import timedelta
 from heapq import heappop
 from typing import cast
 
-import heapdict
 import faust
-from faust import SlidingWindow
 from faust.types.windows import WindowT
 
+broker = os.environ['KAFKA_BOOTRSTRAP_SERVER']
 app = faust.App(
     'aggregator',
-    broker='kafka:9092',
+    broker=broker,
     stream_publish_on_commit=False,
+    producer_max_request_size=1500000,
+    topic_partitions=1,
 )
+
 
 # todo wait for kafka
 
@@ -42,75 +45,21 @@ class Event(faust.Record):
         return self.get_score() < other.get_score()
 
 
-class TrendingHeap(object):
-    """ use a heapdict of fixed size to store the top events
-        more for heapdict: https://www.geeksforgeeks.org/priority-queue-using-queue-and-heapdict-module-in-python/
-     """
+def get_json(data):
+    result = {'id': str(uuid.uuid4()), 'relation_type': 'discusses', 'state': 'aggregated',
+              'created_at': '{0:%Y-%m-%dT%H:%M:%SZ}'.format(datetime.datetime.now())}
+    aggregated_data = {}
+    i = 0
+    for item in data:
+        aggregated_data[i] = {
+            'doi': item[0],
+            'score': item[1]
+        }
+        i += 1
+    result['aggregated_data'] = aggregated_data
+    # print(len(result))
+    return result
 
-    size = 10
-
-    def __init__(self):
-        """ init an empty heapdict """
-        self.heap = heapdict.heapdict()
-
-    def update(self, key, window):
-        """ update the heapdict, return true if changes, false if not
-
-        return true if full and update happened
-         """
-        # update heap data
-        for k in self.heap.keys():
-            self.heap[k] = window[k].delta(timedelta(minutes=60))
-
-        if self.contains(key):
-            return self.is_full()
-        else:
-            score = window[key].delta(timedelta(minutes=60))
-            if not self.is_full():
-                self.heap[key] = score
-                return self.is_full()
-            else:
-                if score > self.get_lowest_score():
-                    self.heap.popitem()
-                    self.heap[key] = score
-                    return self.is_full()
-        return False
-
-    def get_items(self):
-        """ return the item in the heap """
-        return self.heap.items()
-
-    def get_lowest_score(self):
-        """ return the lowest score in this heap """
-        return self.heap.peekitem()[1]
-
-    def contains(self, key):
-        """ check if an event is already in this heap """
-        if key in self.heap:
-            return True
-        return False
-
-    def is_empty(self):
-        """ check if heap is empty """
-        return len(self.heap) == 0
-
-    def is_full(self):
-        """ check if heap is full, meaning its length is the desired size """
-        return len(self.heap) == self.size
-
-    def get_json(self):
-        heap_copy = copy.deepcopy(self.heap)
-        result = {'id': str(uuid.uuid4()), 'relation_type': 'discusses', 'state': 'aggregated',
-                  'created_at': '{0:%Y-%m-%dT%H:%M:%SZ}'.format(datetime.datetime.now())}
-        aggregated_data = {}
-        while heap_copy:
-            item = heap_copy.popitem()
-            aggregated_data[len(heap_copy)] = {
-                'doi': item[0],
-                'score': item[1]
-            }
-        result['aggregated_data'] = aggregated_data
-        return result
 
 def _custom_del_old_keys(self) -> None:
     window = cast(WindowT, self.window)
@@ -120,11 +69,12 @@ def _custom_del_old_keys(self) -> None:
                 timestamps[0],
                 self._partition_latest_timestamp[partition]):
             timestamp = heappop(timestamps)
-            keysList = [self._partition_timestamp_keys.get((partition, window_range[1])) for window_range in self._window_ranges(timestamp) ]
+            keysList = [self._partition_timestamp_keys.get((partition, window_range[1])) for window_range in
+                        self._window_ranges(timestamp)]
             keys_to_remove = self._partition_timestamp_keys.pop(
                 (partition, timestamp), None)
             if keys_to_remove:
-                windowData = [ item for keys in keysList for key in keys for item in self.data.get(key, None)]
+                windowData = [item for keys in keysList for key in keys for item in self.data.get(key, None)]
                 for key in keys_to_remove:
                     value = self.data.pop(key, None)
                     if key[1][0] > self.last_closed_window:
@@ -136,26 +86,28 @@ def _custom_del_old_keys(self) -> None:
 
 
 # def window_processor(key, event):
-    # print(key)
-    # print(event)
-    # print('window end')
+# print(key)
+# print(event)
+# print('window end')
 
 
 processed_topic = app.topic('events_processed-discusses', value_type=Event)
 aggregated_topic = app.topic('events_aggregated')
 # count_table = app.Table('count_processed_events', default=int).hopping(10, 5, expires=timedelta(minutes=10))
 # time in seconds, first is window size, second is time between creation
+time_main = 6
 
-score_table = app.Table('score_processed_events', default=int)\
-    .hopping(timedelta(minutes=60), timedelta(minutes=1), expires=timedelta(minutes=70))
+score_table = app.Table('score_processed_events', default=int, partitions=1) \
+    .hopping(timedelta(minutes=60 * time_main), timedelta(minutes=5), expires=timedelta(minutes=60 * time_main + 5), key_index=True)
+
+
 # on_window_close=window_processor
 
 # score_table._del_old_keys = types.MethodType(_custom_del_old_keys, score_table)
 
-trending = TrendingHeap()
-
 
 # todo save them using current
+# todo only use one partition
 # but in own table not window
 # or all in heap?
 # remove elements on window close, using window delta?
@@ -174,11 +126,16 @@ async def aggregate(events):
     async for event in events.group_by(Event.obj_id):
         # count_table[event.obj_id] += 1
         score_table[event.obj_id] += event.get_score()
-        if trending.update(event.obj_id, score_table):
-            # print('update')
-            if time.time() - time_last_publish > 5:
-                time_last_publish = time.time()
-                yield trending.get_json()
+        if time.time() - time_last_publish > 5:
+            time_last_publish = time.time()
+            # yield trending.get_json()
+            time_event = time.time()
+            list_of_trends = list(score_table.relative_to_now().items().delta(timedelta(minutes=60 * time_main)))
+            result = get_json(sorted(list_of_trends, key=lambda k: k[1], reverse=True)[:15])
+            print(len(list_of_trends) / ((time.time() - time_event) * 1000))
+            yield result
+            #     'trends': l,
+            # }
         # check if obj_id in heap
         # true update
         # false
@@ -190,6 +147,7 @@ async def aggregate(events):
         # print(event.obj_id)
         # print(count_table[event.obj_id].current())
         # print(score_table[event.obj_id].current())
+
 
 # @app.agent(topic_to_read_from, sink=[destination_topic])
 # async def fetch(records):
